ba90c9b5d733e0da9b8c0c5d3f8b3ec38d9dc41b
Liam McLeod <l.mcleod95@gmail.com>
2019-12-27 22:16:13 +0000

    fixed right click tray, + graphics bug from electron update

--- main.js
+++ main.js
@@ -4,33 +4,32 @@ Liam McLeod, 2018.
 
 //* Modules to control application life and create native browser window
 const {
-    app,
-    BrowserWindow,
-    dialog,
-    globalShortcut,
-    Menu,
-    Notification,
-    Tray
-
-} = require('electron');
+	app,
+	BrowserWindow,
+	dialog,
+	globalShortcut,
+	Menu,
+	Notification,
+	Tray
+} = require("electron");
 
 //* Misc Imports
-const path = require('path');
-const fs = require('fs');
-const electronLocalshortcut = require('electron-localshortcut');
+const path = require("path");
+const fs = require("fs");
+const electronLocalshortcut = require("electron-localshortcut");
 
 //* My Modules
-const crypto = require('./modules/crypto');
-const steam = require('./modules/steam');
-const account = require('./modules/account');
-const init = require('./modules/init');
+const crypto = require("./modules/crypto");
+const steam = require("./modules/steam");
+const account = require("./modules/account");
+const init = require("./modules/init");
 
 //* Stores
 var accountsStore = [];
-var userId = '';
+var userId = "";
 
 //* TODOS
-//todo look into below reg to remember based upon the 
+//todo look into below reg to remember based upon the
 //todo value of index.remember
 //? https://github.com/CatalystCode/windows-registry-node
 //reg add "HKCU\Software\Valve\Steam" / v AutoLoginUser / t REG_SZ / d % username % /f
@@ -50,71 +49,67 @@ var userId = '';
 
 //todo maybe adjust  crypto to use username, encrypt entire JSON output???
 
-/** 
- * Import my own log system, just to ensure 
+/**
+ * Import my own log system, just to ensure
  * nothing makes it out of the application
  * in production
  */
-const {
-    log,
-    isDebug
-} = require('./modules/log');
+const { log, isDebug } = require("./modules/log");
 
-/** 
+/**
  * Keep a global reference of the window object, if you don't, the window will
  * be closed automatically when the JavaScript object is garbage collected.
  */
 let mainWindow;
-
+app.disableHardwareAcceleration();
 function createWindow() {
-    //* Create the browser window.
-    mainWindow = new BrowserWindow({
-        width: 480,
-        height: 380,
-        resizable: isDebug(),
-        fullscreenable: false,
-        icon: __dirname + "/favicon-32x32.png",
-        title: "Steam Switch",
-        backgroundColor: '#303030',
-        webPreferences: {
-            darkTheme: true,
-            fullscreenable: false,
-            devTools: isDebug(),
-            nodeIntegration: true,
-        }
-    });
+	//* Create the browser window.
+	mainWindow = new BrowserWindow({
+		width: 480,
+		height: 380,
+		resizable: isDebug(),
+		fullscreenable: false,
+		icon: __dirname + "/favicon-32x32.png",
+		title: "Steam Switch",
+		backgroundColor: "#303030",
+		webPreferences: {
+			darkTheme: true,
+			fullscreenable: false,
+			devTools: isDebug(),
+			nodeIntegration: true
+		}
+	});
 
-    //* and load the index.html of the app.
-    mainWindow.loadFile('index.html');
+	//* and load the index.html of the app.
+	mainWindow.loadFile("index.html");
 
-    //* Misc Window  things
-    mainWindow.setMenu(null);
+	//* Misc Window  things
+	mainWindow.setMenu(null);
 
-    //* Open the DevTools.
-    //! But must be off when debugging with VS Code
-    if (isDebug()) {
-        mainWindow.webContents.openDevTools();
-    }
+	//* Open the DevTools.
+	//! But must be off when debugging with VS Code
+	if (isDebug()) {
+		mainWindow.webContents.openDevTools();
+	}
 
+	//* Main Window Event Listeners
+	mainWindow.on("minimize", e => {
+		e.preventDefault();
+		//* Create tray icon,
+		createTray(e);
+		//* Hide Window
+		mainWindow.hide();
+	});
 
-    //* Main Window Event Listeners
-    mainWindow.on('minimize', (e) => {
-        e.preventDefault();
-        //* Create tray icon,
-        createTray(e);
-        //* Hide Window
-        mainWindow.hide();
-    });
-
-    //* Emitted when the window is closed.
-    mainWindow.on('closed', () => {
-        /** 
-         * Dereference the window object, usually you would store windows
-         * in an array if your app supports multi windows,  is the time
-         * when you should delete the corresponding element.
-         */
-        mainWindow = null;
-    });
+	//* Emitted when the window is closed.
+	mainWindow.on("closed", () => {
+		/**
+		 * Dereference the window object, usually you would store windows
+		 * in an array if your app supports multi windows,  is the time
+		 * when you should delete the corresponding element.
+		 */
+		mainWindow = null;
+	});
 }
 
 /**
@@ -122,27 +117,27 @@ function createWindow() {
  * initialization and is ready to create browser windows.
  * Some APIs can only be used after  event occurs.
  */
-app.on('ready', () => {
-    log("Debug: " + isDebug());
-    //* Create folder if necessary
-    init.checkFirstRun();
-    //* Retrieve data
-    accountsStore = account.updateStore();
-    //* Get Id
-    userId = crypto.getId();
-    //* Create window
-    createWindow();
+app.on("ready", () => {
+	log("Debug: " + isDebug());
+	//* Create folder if necessary
+	init.checkFirstRun();
+	//* Retrieve data
+	accountsStore = account.updateStore();
+	//* Get Id
+	userId = crypto.getId();
+	//* Create window
+	createWindow();
 
-    //* Global 
-    if (isDebug()) {
-        globalShortcut.register('CommandOrControl+R', () => {
-            mainWindow.reload();
-        });
-    }
+	//* Global
+	if (isDebug()) {
+		globalShortcut.register("CommandOrControl+R", () => {
+			mainWindow.reload();
+		});
+	}
 
-    //* Attach local Shortcuts
-    //todo look into solving it
-    /**
+	//* Attach local Shortcuts
+	//todo look into solving it
+	/**
      *!Seem to get this
      *!https: //github.com/parro-it/electron-localshortcut/issues/59
     electronLocalshortcut.register(mainWindow, ['Ctrl+R', 'F5'], () => {
@@ -151,233 +146,237 @@ app.on('ready', () => {
      */
 });
 
-app.once('ready-to-show', () => {
-    // mainWindow.show();
+app.once("ready-to-show", () => {
+	// mainWindow.show();
 });
 
 //* Quit when all windows are closed.
-app.on('window-all-closed', () => {
-    /** 
-     * On OS X it is common for applications and their menu bar
-     * to stay active until the user quits explicitly with Cmd + Q
-     */
-    if (process.platform !== 'darwin') {
-        app.quit();
-    }
+app.on("window-all-closed", () => {
+	/**
+	 * On OS X it is common for applications and their menu bar
+	 * to stay active until the user quits explicitly with Cmd + Q
+	 */
+	if (process.platform !== "darwin") {
+		app.quit();
+	}
 });
 
-app.on('activate', () => {
-    /**
-     * On OS X it's common to re-create a window in the app when the
-     * dock icon is clicked and there are no other windows open.
-     */
-    if (mainWindow === null) {
-        createWindow();
-    }
+app.on("activate", () => {
+	/**
+	 * On OS X it's common to re-create a window in the app when the
+	 * dock icon is clicked and there are no other windows open.
+	 */
+	if (mainWindow === null) {
+		createWindow();
+	}
 });
 
-/** 
+/**
  *  In file you can include the rest of your app's specific main process
  *  code. You can also put them in separate files and require them here.
  */
 
 /**
  * @param  e  Object  Event
- * 
- * Create tray icon complete with  
+ *
+ * Create tray icon complete with
  * menu items + accounts if exists
  */
 function createTray(e) {
-    var tray = new Tray(path.join(__dirname, "favicon.ico"));
-    //* Default Context Menu
-    var menuItems = [{
-            label: 'Show',
-            click: function() {
-                //* Show Window
-                mainWindow.show();
-                //* Remove Tray Icon
-                tray.destroy();
-            }
-        },
-        {
-            label: 'Quit',
-            click: function() {
-                //* Remove Tray Icon
-                tray.destroy();
-                //* Quit
-                app.quit();
-            }
-        },
-    ];
-    //* Generate additional menu items
-    if (accountsStore) {
-        accountsStore.forEach(function(item) {
-            menuItems.unshift({
-                label: 'Launch ' + item.name,
-                click: function() {
-                    //* Launch Steam Account with ID
-                    launchSteam(item.id);
-                }
-            });
-        });
-    }
+	var tray = new Tray(path.join(__dirname, "favicon.ico"));
+	//* Default Context Menu
+	var menuItems = [
+		{
+			label: "Show",
+			click: function() {
+				//* Show Window
+				mainWindow.show();
+				//* Remove Tray Icon
+				tray.destroy();
+			}
+		},
+		{
+			label: "Quit",
+			click: function() {
+				//* Remove Tray Icon
+				tray.destroy();
+				//* Quit
+				app.quit();
+			}
+		}
+	];
+	//* Generate additional menu items
+	if (accountsStore) {
+		accountsStore.forEach(function(item) {
+			menuItems.unshift({
+				label: "Launch " + item.name,
+				click: function() {
+					//* Launch Steam Account with ID
+					launchSteam(item.id);
+				}
+			});
+		});
+	}
 
-    //* Create Menu
-    var contextMenu = Menu.buildFromTemplate(menuItems);
-    tray.setContextMenu(contextMenu);
+	//* Create Menu
+	var contextMenu = Menu.buildFromTemplate(menuItems);
+	tray.setContextMenu(contextMenu);
 
-    //* Add Listeners
-    tray.on('right-click', (e) => {
-        tray.popUpContextMenu(contextMenu);
-    });
-    tray.on('double-click', (e) => {
-        mainWindow.show();
-        tray.destroy();
-    });
+	//* Add Listeners
+	tray.on("right-click", e => {
+		tray.popUpContextMenu(contextMenu);
+	});
+	tray.on("double-click", e => {
+		mainWindow.show();
+		tray.destroy();
+	});
 }
 
 /**
  * Simple Notification from Electron Docs
  */
 function createNotification() {
-    if (Notification.isSupported()) {
-        var notification = new Notification('Launching Steam...', {
-            icon: __dirname + '/favicon-32x32.png',
-            body: 'Steam is launching, this shouldn\'t take long...'
-        });
-        notification.show();
+	if (Notification.isSupported()) {
+		var notification = new Notification("Launching Steam...", {
+			icon: __dirname + "/favicon-32x32.png",
+			body: "Steam is launching, this shouldn't take long..."
+		});
+		notification.show();
 
-        notification.onclick = () => {
-            notification.hide();
-        };
-    }
+		notification.onclick = () => {
+			notification.hide();
+		};
+	}
 }
-
 
 /**
  * Simple Error Dialogue
  */
 function createError(title, message) {
-    var options = {
-        title: title,
-        type: 'error',
-        buttons: ['OK'],
-        message: message
-    };
-    dialog.showMessageBox(mainWindow, options, () => {});
+	var options = {
+		title: title,
+		type: "error",
+		buttons: ["OK"],
+		message: message
+	};
+	dialog.showMessageBox(mainWindow, options, () => {});
 }
 
 /**
  * @param  id  String  ID of account
- * 
+ *
  * Launches Steam logging into account with ID
  */
 function launchSteam(id) {
-    if (!id) {
-        //! ERROR MSG HERE
-        return;
-    } else {
-        //*Begin with Steam
-        steam.steamExists(id, function(id, steamExists) {
-            if (steamExists) {
-                steam.closeSteam(id, function(id) {
-                    steam.openSteam(id);
-                });
-            }
-            steam.openSteam(id);
-        });
-    }
+	if (!id) {
+		//! ERROR MSG HERE
+		return;
+	} else {
+		//*Begin with Steam
+		steam.steamExists(id, function(id, steamExists) {
+			if (steamExists) {
+				steam.closeSteam(id, function(id) {
+					steam.openSteam(id);
+				});
+			}
+			steam.openSteam(id);
+		});
+	}
 }
 
 //* Steam Events
-const steamOpen = require('./modules/steam').openEvent;
-const steamClose = require('./modules/steam').closeEvent;
-steamOpen.on('steamOpen', (e) => {
-    mainWindow.setOverlayIcon(path.join(__dirname, "public/greenoverlay.png"), 'Steam Switch');
+const steamOpen = require("./modules/steam").openEvent;
+const steamClose = require("./modules/steam").closeEvent;
+steamOpen.on("steamOpen", e => {
+	mainWindow.setOverlayIcon(
+		path.join(__dirname, "public/greenoverlay.png"),
+		"Steam Switch"
+	);
 });
 
-steamClose.on('steamClose', (e) => {
-    mainWindow.setOverlayIcon(path.join(__dirname, "public/greenoverlay.png"), 'Steam Switch');
+steamClose.on("steamClose", e => {
+	mainWindow.setOverlayIcon(
+		path.join(__dirname, "public/greenoverlay.png"),
+		"Steam Switch"
+	);
 });
 
 //* Event Listeners & Inter-Proc Comms
-const {
-    ipcMain
-} = require('electron');
+const { ipcMain } = require("electron");
 
 //* Listener on the mainProcess to recieve renderProcess data
-ipcMain.on('request-mainprocess-action', (event, proc) => {
-    //* Basic controller for 
-    if (proc) {
-        if (proc.id) {
-            //* Launch Steam,
-            launchSteam(proc.id);
-            //* Create tray icon,
-            createTray();
-            //* Hide Window,
-            mainWindow.hide();
-        }
-        if (proc.post) {
-            //* Generate Id
-            do {
-                proc.post.id = crypto.generateId(2, 'hex');
-            } while (account.checkUnique(proc.post.id) !== true);
+ipcMain.on("request-mainprocess-action", (event, proc) => {
+	//* Basic controller for
+	if (proc) {
+		if (proc.id) {
+			//* Launch Steam,
+			launchSteam(proc.id);
+			//* Create tray icon,
+			createTray();
+			//* Hide Window,
+			mainWindow.hide();
+		}
+		if (proc.post) {
+			//* Generate Id
+			do {
+				proc.post.id = crypto.generateId(2, "hex");
+			} while (account.checkUnique(proc.post.id) !== true);
 
-            //* Generate & Store Key
-            proc.post.key = crypto.generateId(20);
-            //* Hash Key 
-            var key = crypto.createKey(proc.post.key);
+			//* Generate & Store Key
+			proc.post.key = crypto.generateId(20);
+			//* Hash Key
+			var key = crypto.createKey(proc.post.key);
 
-            //* Encrypt
-            proc.post.password = crypto.encryptPass(key, proc.post.password);
+			//* Encrypt
+			proc.post.password = crypto.encryptPass(key, proc.post.password);
 
-            //TODO ERROR CHECK
+			//TODO ERROR CHECK
 
-            account.storeAccount(proc.post);
-            accountsStore = account.updateStore();
-        }
-        if (proc.get) {
-            var edit = account.getAccountById(proc.get);
-            mainWindow.webContents.send('edit', edit);
-        }
-        if (proc.put) {
-            //* Generate & Store Key
-            proc.put.key = crypto.generateId(20);
-            //* Hash Key 
-            var key = crypto.createKey(proc.put.key);
-            //* Encrypt
-            proc.put.password = crypto.encryptPass(key, proc.put.password);
-            //* Store
-            account.editAccount(proc.put);
-            accountsStore = account.updateStore();
-        }
-        if (proc.delete) {
-            account.deleteAccount(proc.delete);
-            accountsStore = account.updateStore();
-        }
-    }
-    // mainWindow.reload();
+			account.storeAccount(proc.post);
+			accountsStore = account.updateStore();
+		}
+		if (proc.get) {
+			var edit = account.getAccountById(proc.get);
+			mainWindow.webContents.send("edit", edit);
+		}
+		if (proc.put) {
+			//* Generate & Store Key
+			proc.put.key = crypto.generateId(20);
+			//* Hash Key
+			var key = crypto.createKey(proc.put.key);
+			//* Encrypt
+			proc.put.password = crypto.encryptPass(key, proc.put.password);
+			//* Store
+			account.editAccount(proc.put);
+			accountsStore = account.updateStore();
+		}
+		if (proc.delete) {
+			account.deleteAccount(proc.delete);
+			accountsStore = account.updateStore();
+		}
+	}
+	// mainWindow.reload();
 });
 
 //* DOM ready, Get accounts to display
-ipcMain.on('dom-ready', () => {
-    var accounts = account.getAccount();
-    /** 
-     * Flag for rendering engine to know where templates are stored based 
-     * as this differs between production and dev due to compiling
-     */
-    accounts.unshift(process.env.NODE_ENV);
-    mainWindow.webContents.send('ping', accounts);
+ipcMain.on("dom-ready", () => {
+	var accounts = account.getAccount();
+	/**
+	 * Flag for rendering engine to know where templates are stored based
+	 * as this differs between production and dev due to compiling
+	 */
+	accounts.unshift(process.env.NODE_ENV);
+	mainWindow.webContents.send("ping", accounts);
 });
 
 //* Request to refresh
-ipcMain.on('refresh', () => {
-    mainWindow.reload();
+ipcMain.on("refresh", () => {
+	mainWindow.reload();
 });
 
 //* Error feedback
-ipcMain.on('show-error', (e, error) => {
-    createError(error.title, error.message);
+ipcMain.on("show-error", (e, error) => {
+	createError(error.title, error.message);
 });
 
 //* main process, for example app/main.js
